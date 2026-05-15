018c6d40c3a88c8a0ac9b29912cd635ca9b2c1d1
Carlos Aguiar <caduaguiar965@gmail.com>
2026-05-14 21:04:20 -0300

    added main modules and API
    Added(Product, Client, SaleItem, Sale) and the Web API has been registered

---
+++ Mini-ERP/Mini-ERP/Models/Client.cs
@@ -0,0 +1,10 @@
+﻿namespace Mini_ERP.Models;
+
+public class Client
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Cpf { get; set; } = string.Empty;
+
+}
---
+++ Mini-ERP/Mini-ERP/Models/Sale.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mini_ERP.Models;
+
+public class Sale
+{
+    public int Id { get; set; }
+    public DateTime SaleDate { get; set; } = DateTime.Now;
+    
+    public int ClientId { get; set; }
+    public Client? Client { get; set; }
+
+    public decimal TotalAmount { get; set; }
+    public List<SaleItem> Items { get; set; } = new List<SaleItem>();
+}
